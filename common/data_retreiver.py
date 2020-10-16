65c917b5aa3fae78bd7405e470874b74cc0b96f4
Ben Kizaric <bakizaric@wisc.edu>
2020-10-16 18:32:20 -0500

    Adjusted for inflation and market conditions in sales prices

--- common/data_retreiver.py
+++ common/data_retreiver.py
@@ -20,17 +20,16 @@ def download_file(url, path):
 DATA_PATH = 'temp_data'
 
 DATASETS = {
-  "all_parcel_raw.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_raw.csv",
   "all_parcel_assessments.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_assessments.csv",
   "all_parcel_names.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_names.csv",
   "all_parcel_sales.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_sales.csv",
   "all_parcel_locations.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_locations.csv",
-  "single_family_assessments.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single%20family%20all_parcel_assessments.csv",
   "single_family_locations.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single_family_locations.csv",
   "single_family_names.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single_family_names.csv",
   "single_family_sales.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single_family_sales.csv",
   "all_parcel_core.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/all_parcel_core.csv",
   "single_family_core.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single_family_core.csv",
+  "year_sales.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/year_sales.csv",
 }
 
 def download_datasets():
