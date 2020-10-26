7a0ccf149cbe007b56afa150c83e16585cbb5b96
Ben Kizaric <bakizaric@wisc.edu>
2020-10-25 20:25:06 -0500

    Added Current Area Evaluations

--- common/data_retreiver.py
+++ common/data_retreiver.py
@@ -1,6 +1,7 @@
 import os
 import urllib3
 import pandas as pd
+import geopandas as gpd
 
 def download_file(url, path):
     chunk_size = 255
@@ -32,11 +33,42 @@ DATASETS = {
   "year_sales.csv": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/year_sales.csv",
 }
 
+FAMILY_DATASETS = {
+    "single_family_core.csv": DATASETS["single_family_core.csv"],
+    "year_sales.csv": DATASETS["year_sales.csv"],
+    "single_family_sales.csv": DATASETS["single_family_sales.csv"],
+    "year_sales.csv": DATASETS["year_sales.csv"]
+}
+
+GEO_DATA = {
+    "single_family_areas_shapes.zip": "https://raw.githubusercontent.com/madison-housing-cs638/data/master/single_family_areas_shapes.zip"
+}
+
 def download_datasets():
   for f in DATASETS:
       filePath = os.path.join(os.pardir, DATA_PATH, f)
       fileUrl = DATASETS[f]
       download_file(fileUrl, filePath)
+
+def download_family_datasets():
+    for f in FAMILY_DATASETS:
+      filePath = os.path.join(os.pardir, DATA_PATH, f)
+      fileUrl = FAMILY_DATASETS[f]
+      download_file(fileUrl, filePath)
+
+def download_geo_data():
+    for f in GEO_DATA:
+      filePath = os.path.join(os.pardir, DATA_PATH, f)
+      fileUrl = GEO_DATA[f]
+      download_file(fileUrl, filePath)
+
+def load_geo_datasets(*datasets):
+    result = []
+    for datasetName in datasets:
+        fullPath = os.path.join(os.pardir, DATA_PATH, datasetName + '.zip')
+        geo_df = gpd.read_file(f"zip://{fullPath}")
+        result.append(geo_df)
+    return tuple(result)
 
 def load_csv_datasets(*datasets): #Returns a tupple of pandas dataframes.
     result = []
